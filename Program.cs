d940f9704c650218032d52f3e44e376588cd3b04
nobushi95 <nobushi1121@gmail.com>
2023-09-18 17:22:53 +0900

    プロジェクト構成修正

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
-﻿namespace dotnet_DateTime_ToString
+﻿namespace dotnet_DateTime_ToString;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
