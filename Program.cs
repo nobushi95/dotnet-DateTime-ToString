b24740d8a1171f9d50b216f10f8ba0d1d2b1fe78
nobushi95 <nobushi1121@gmail.com>
2023-09-18 17:31:10 +0900

    DateTime表示の実装

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
-﻿namespace dotnet_DateTime_ToString;
+﻿using System.Globalization;
+
+namespace dotnet_DateTime_ToString;
 
 internal class Program
 {
+    private static readonly string Format = "yyyy/MM/dd HH:mm:ss";
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+            var now = DateTime.UtcNow;
+            Console.WriteLine($"ja-JP:     {now.ToString(Format, new CultureInfo("ja-JP"))}");
+            Console.WriteLine($"en-US:     {now.ToString(Format, new CultureInfo("en-US"))}");
+            Console.WriteLine($"invariant: {now.ToString(Format, CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine("if you want to exit program, type \"q\".");
+
+            var c = Console.ReadLine();
+            if (c is "q" or "exit" or "quit")
+                break;
+        }
     }
 }
